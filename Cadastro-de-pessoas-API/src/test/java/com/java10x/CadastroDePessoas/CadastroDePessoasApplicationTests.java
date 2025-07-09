b3e0d13709481f3a01d0a805cf9850b5d769a05b
Diego Apicello <manoburn@gmail.com>
2025-07-09 11:05:30 -0300

    first commit

---
+++ Cadastro-de-pessoas-API/src/test/java/com/java10x/CadastroDePessoas/CadastroDePessoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java10x.CadastroDePessoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CadastroDePessoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
